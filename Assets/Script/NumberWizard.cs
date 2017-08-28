7d8da589b31a6c4fefcd9a985666a839c9a37df1
Ajay Rawat <ajay.rawat@cowrks.com>
2017-08-29 01:07:18 +0530

    Number Function updated

--- Assets/Script/NumberWizard.cs
+++ Assets/Script/NumberWizard.cs
@@ -6,12 +6,24 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		print("Welcome to Number Wizard ");
+		print("Pick a number in your head ! but don't tell me ");
+		int max = 2000;
+		int min = 1;
+		print("The highest no can be " + max);
+		print("The lowest no can be " + min);
+
+
 	}
+
 	
 	// Update is called once  per frame
 		
 	void Update () {
+		if( Input.GetKeyDown(KeyCode.UpArrow)){
+			print("Up arow pressed");
+
+		}
 		
 	}
 }
